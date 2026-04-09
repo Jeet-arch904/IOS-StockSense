2e2c7f32de74b8b93c27b394374d37fae0eac5e7
jette25-cyber <jette25@vt.edu>
2026-04-09 17:42:22 -0400

    Initial Commit

---
+++ Invst/InvstApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvstApp.swift
+//  Invst
+//
+//  Created by Jett Ehlert on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct InvstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
