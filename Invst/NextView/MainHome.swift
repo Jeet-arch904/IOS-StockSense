df1f0875cd874fcfa1c254b3000b9413848431ea
jette25-cyber <jette25@vt.edu>
2026-05-02 19:06:19 -0400

    Change transition between screens and comments

---
+++ Invst/NextView/MainHome.swift
@@ -0,0 +1,8 @@
+//
+//  MainHome.swift
+//  Invst
+//
+//  Created by Jett Ehlert on 4/24/26.
+//
+
+import Foundation
---
+++ Invst/NextView/MainHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  MainHomeView.swift
+//  Invst
+//
+//  Created by Jett Ehlert on 4/24/26.
+//
+
+import SwiftUI
+
+struct MainHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainHomeView()
+}
